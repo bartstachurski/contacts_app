2f84ef0d145b4b1ca9a3eae4fa62b18e61b4497c
bartstachurski <bart.stachurski@gmail.com>
2019-07-01 22:35:40 -0400

    create model method returning full name of contact and modified show view to show the full name and friendly_updated_at info for a contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   def friendly_updated_at
     return updated_at.strftime("Updated on %m/%d/%Y at %I:%M%p")
   end
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
 end
