40a9d90a611f84518d4e78485ae995b8c265adaf
bartstachurski <bart.stachurski@gmail.com>
2019-07-01 20:23:10 -0400

    creates restful show route to show specific contact by id

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
     get '/contacts' => 'contacts#index'
 
+    get '/contacts/:id' => 'contacts#show'
+
     get "/all_contacts" => "contacts#all_contacts"
   end
 end
