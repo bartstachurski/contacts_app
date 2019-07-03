01ff01081fd152c8a496edd5a771e39f86b73006
bartstachurski <bart.stachurski@gmail.com>
2019-07-02 22:12:20 -0400

    adds bio attribute to contacts model and amended create and update actions to accept and show the bio and middle name attributes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -15,7 +15,9 @@ class Api::ContactsController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-      phone_number: params[:phone_number]
+      phone_number: params[:phone_number],
+      middle_name: params[:middle_name],
+      bio: params[:bio]
       )
     @contact.save
     render 'create.json.jb'
@@ -33,6 +35,8 @@ class Api::ContactsController < ApplicationController
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @phone_number.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
+    @contact.bio = params[:bio] || @contact.bio
     @contact.save
     render 'update.json.jb'
   end
